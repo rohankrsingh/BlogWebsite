902b37f6415d54011a613f7e849ab9a147260fe2
Rohan Kumar Singh <rohankrsingh125@gmail.com>
2025-01-15 18:49:29 +0530

    transition from TinyMCE to mdx Editor

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -3,7 +3,6 @@ import appwriteService from "../appwrite/config"
 import {Link} from 'react-router-dom'
 
 function PostCard({$id, title, featuredImage}) {
-    console.log(featuredImage);
     
   return (
     <Link to={`/post/${$id}`}>
