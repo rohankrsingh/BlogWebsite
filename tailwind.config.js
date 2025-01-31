774de2735dcc86146b39c9f92bfbba6a873d4770
Rohan Kumar Singh <rohankrsingh125@gmail.com>
2025-01-31 22:10:07 +0530

    post page optimization

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-				poppins: ['-apple-system','"Poppins"', "sans-serif"],
+				poppins: ['"Poppins"', "sans-serif"],
 				sourceCode: ['"Source Code Pro"', "monospace"],
 				sans: ['"Open Sans"', "sans-serif"], // Set Open Sans as the default sans-serif font
 				openSans: ['"Open Sans"', "sans-serif"],
