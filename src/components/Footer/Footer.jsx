0e69ec07616bea54a7707d850880e40e2319d90a
Rohan Kumar Singh <rohankrsingh125@gmail.com>
2025-02-08 22:47:51 +0530

    `Refactor Footer component layout and add social media links`

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,142 +2,36 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../Logo'
 import { Card } from '../ui'
+import { Github, Heart, Linkedin } from 'lucide-react'
+import { FaTelegram } from "react-icons/fa";
 function Footer() {
-  return (
-    <section className="relative overflow-hidden ">
-            <Card className="relative w-full  py-10 px-4">
-                <div className="-m-6 flex flex-wrap">
-                    <div className="w-full p-6 md:w-1/2 lg:w-5/12">
-                        <div className="flex h-full flex-col justify-between">
-                            <div className="mb-4 inline-flex items-center">
-                                <Logo width="100px" />
-                            </div>
-                            <div>
-                                <p className="text-sm text-gray-600">
-                                    &copy; Copyright 2023. All Rights Reserved by DevUI.
-                                </p>
-                            </div>
-                        </div>
+    return (
+        <section className="relative overflow-hidden ">
+            <Card className="relative w-full rounded-none">
+                <div className='flex justify-between items-center py-8 px-6 max-sm:flex-col space-y-4'>
+                    <div className="flex items-center space-x-2">
+                        <Logo />
                     </div>
-                    <div className="w-full p-6 md:w-1/2 lg:w-2/12">
-                        <div className="h-full">
-                            <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-gray-500">
-                                Company
-                            </h3>
-                            <ul>
-                                <li className="mb-4">
-                                    <Link
-                                        className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                                        to="/"
-                                    >
-                                        Features
-                                    </Link>
-                                </li>
-                                <li className="mb-4">
-                                    <Link
-                                        className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                                        to="/"
-                                    >
-                                        Pricing
-                                    </Link>
-                                </li>
-                                <li className="mb-4">
-                                    <Link
-                                        className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                                        to="/"
-                                    >
-                                        Affiliate Program
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                                        to="/"
-                                    >
-                                        Press Kit
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="w-full p-6 md:w-1/2 lg:w-2/12">
-                        <div className="h-full">
-                            <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-gray-500">
-                                Support
-                            </h3>
-                            <ul>
-                                <li className="mb-4">
-                                    <Link
-                                        className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                                        to="/"
-                                    >
-                                        Account
-                                    </Link>
-                                </li>
-                                <li className="mb-4">
-                                    <Link
-                                        className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                                        to="/"
-                                    >
-                                        Help
-                                    </Link>
-                                </li>
-                                <li className="mb-4">
-                                    <Link
-                                        className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                                        to="/"
-                                    >
-                                        Contact Us
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                                        to="/"
-                                    >
-                                        Customer Support
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="w-full p-6 md:w-1/2 lg:w-3/12">
-                        <div className="h-full">
-                            <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-gray-500">
-                                Legals
-                            </h3>
-                            <ul>
-                                <li className="mb-4">
-                                    <Link
-                                        className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                                        to="/"
-                                    >
-                                        Terms &amp; Conditions
-                                    </Link>
-                                </li>
-                                <li className="mb-4">
-                                    <Link
-                                        className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                                        to="/"
-                                    >
-                                        Privacy Policy
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                                        to="/"
-                                    >
-                                        Licensing
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
+                    <nav className=" md:mt-0 flex space-x-6 text-foreground-600">
+                        <h5>Made with <span className='text-red-500'>&#10084;</span> by Rohan</h5>
+                    </nav>
+                    <div className="mt-4 md:mt-0 flex space-x-4">
+                        <a href="#" className=" hover:text-primary-700">
+                            <Github size={20} />
+                        </a>
+                        <a href="#" className=" hover:text-primary-700">
+                            <Linkedin size={20} />
+                        </a>
+                        <a href="#" className=" hover:text-primary-700">
+                            <FaTelegram size={20} />
+                        </a>
                     </div>
                 </div>
+                <p className='text-sm text-muted-foreground text-center mb-4'>Discover insights, stories, and tips from our community.</p>
             </Card>
+
         </section>
-  )
+    )
 }
 
 export default Footer
