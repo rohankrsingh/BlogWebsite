5281696f36c99f6ad7e190164b2a1ada42ce3844
Rohan Kumar Singh <rohankrsingh125@gmail.com>
2025-02-27 18:14:32 +0530

    `Refactor Profile component to remove unused code and improve readability`

--- src/components/settings/Profile.jsx
+++ src/components/settings/Profile.jsx
@@ -16,14 +16,10 @@ import Loader from "../Loader";
 
 const profileFormSchema = z.object({
     username: z.string()
-        .min(2, "Username must be atleast 2 characters.")
-        .max(36, "Username must not be more than 36 characters"),
-    name: z.string().min(2, {
-        message: "Name must be at least 2 characters.",
-    }),
-    email: z.string().email({
-        message: "Please enter a valid email address.",
-    }),
+        .min(2, "Username must be at least 2 characters.")
+        .max(36, "Username must not be more than 36 characters."),
+    name: z.string().min(2, "Name must be at least 2 characters."),
+    email: z.string().email("Please enter a valid email address."),
     bio: z.string().max(100).optional(),
     website: z.string().url().optional(),
     location: z.string().optional(),
@@ -33,10 +29,9 @@ export default function Profile() {
     const [selectedAvatar, setSelectedAvatar] = useState("");
     const [error, setError] = useState("");
     const [loading, setLoading] = useState(false);
+    const [userProfile, setUserProfile] = useState(null);
     const userId = useSelector((state) => state.auth.userData.$id);
-    const userEmail = useSelector((state) => state.auth.userData).email;
-    console.log(userEmail);
-
+    const userEmail = useSelector((state) => state.auth.userData.email);
 
     const form = useForm({
         resolver: zodResolver(profileFormSchema),
@@ -51,24 +46,21 @@ export default function Profile() {
         },
     });
 
-
     useEffect(() => {
         const fetchUserProfile = async () => {
             setLoading(true);
             try {
-                
                 const details = await service.getUserProfile(userId);
                 if (details) {
-                    form.reset({
+                    setUserProfile({
                         avatar: details.avatar || "",
                         name: details.name || "",
-                        username: details.username || "",
-                        // email: userEmail || details.email || "",
+                        username: details.username || userEmail || "",
                         bio: details.bio || "",
                         website: details.website || "",
                         location: details.location || "",
                     });
-                    setSelectedAvatar(details.avatar)
+                    setSelectedAvatar(details.avatar);
                 }
             } catch {
                 setError("Failed to fetch user profile.");
@@ -77,27 +69,29 @@ export default function Profile() {
             }
         };
         fetchUserProfile();
-    }, [userId, form]);
+    }, [userId, userEmail]);
 
-const onSubmit = async (data) => {
-    setLoading(true);
-    try {
-        const existingUsernames = await service.getUsernames(data.username);
-        if (existingUsernames && existingUsernames.length > 0) {
-            const existingUserId = existingUsernames[0].$id;
-            if (existingUserId !== userId) {
+    useEffect(() => {
+        if (userProfile) {
+            form.reset(userProfile);
+        }
+    }, [userProfile, form]);
+
+    const onSubmit = async (data) => {
+        setLoading(true);
+        try {
+            const existingUsername = await service.getUsernames(data.username);
+            if (existingUsername && existingUsername.length > 0 && existingUsername[0].$id !== userId) {
                 setError("Username already exists.");
                 return;
             }
-        }
-        await authService.updateAccountName(data.name);
-        data.userId = userId;
-        data.avatar = selectedAvatar;
-        data.bio = data.bio.trim() === "" ? null : data.bio;
-        data.website = data.website.trim() === "" ? null : data.website;
-        data.location = data.location.trim() === "" ? null : data.location;
-        await service.updateUserProfile(data.userId, data.username, data.name, data.email, data.bio, data.location, data.avatar, data.website);
-
+            await authService.updateAccountName(data.name);
+            data.userId = userId;
+            data.avatar = selectedAvatar;
+            data.bio = data.bio.trim() === "" ? null : data.bio;
+            data.website = data.website.trim() === "" ? null : data.website;
+            data.location = data.location.trim() === "" ? null : data.location;
+            await service.updateUserProfile(data.userId, data.username, data.name, data.email, data.bio, data.location, data.avatar, data.website);
         } catch (err) {
             setError("Failed to update profile.");
             console.log(err.message);
@@ -107,113 +101,116 @@ const onSubmit = async (data) => {
     };
 
     return (
-        <Card className="max-w-2xl mx-auto space-y-8 p-8">
-            {loading && <Loader />}
-            {error && <p className="text-red-500">{error}</p>}
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                    <div className="space-y-4">
-                        <h2 className="text-2xl font-semibold">Avatar</h2>
-                        <div className="grid gap-4">
-                            <AvatarSelector selectedAvatar={selectedAvatar} onSelect={setSelectedAvatar} />
-                        </div>
-                        <Separator />
-                        <h2 className="text-2xl font-semibold">User</h2>
-                        <div className="grid gap-4">
-                            <FormField
-                                control={form.control}
-                                name="name"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Name</FormLabel>
-                                        <FormControl>
-                                            <Input placeholder="Your name" {...field} />
-                                        </FormControl>
-                                        <FormMessage {...form.error} />
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name="username"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Username</FormLabel>
-                                        <FormControl>
-                                            <Input placeholder="Your username" {...field} />
-                                        </FormControl>
-                                        <FormMessage {...form.error} />
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name="email"
-                                disabled
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Email</FormLabel>
-                                        <FormControl>
-                                            <Input placeholder="email@example.com" {...field} />
-                                        </FormControl>
-                                        <FormMessage {...form.error} />
-                                    </FormItem>
-                                )}
-                            />
-                        </div>
-                    </div>
+        <>
+            {loading ? <Loader /> : (
+                <Card className="max-w-2xl mx-auto space-y-8 p-8">
+                    {error && <p className="text-red-500">{error}</p>}
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                            <div className="space-y-4">
+                                <h2 className="text-2xl font-semibold">Avatar</h2>
+                                <div className="grid gap-4">
+                                    <AvatarSelector selectedAvatar={selectedAvatar} onSelect={setSelectedAvatar} />
+                                </div>
+                                <Separator />
+                                <h2 className="text-2xl font-semibold">User</h2>
+                                <div className="grid gap-4">
+                                    <FormField
+                                        control={form.control}
+                                        name="name"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Name</FormLabel>
+                                                <FormControl>
+                                                    <Input placeholder="Your name" {...field} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                    <FormField
+                                        control={form.control}
+                                        name="username"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Username</FormLabel>
+                                                <FormControl>
+                                                    <Input placeholder="Your username" {...field} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                    <FormField
+                                        control={form.control}
+                                        name="email"
+                                        disabled
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Email</FormLabel>
+                                                <FormControl>
+                                                    <Input placeholder="email@example.com" {...field} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+                            </div>
 
-                    <div className="space-y-4">
-                        <h2 className="text-xl font-semibold">Basic</h2>
-                        <div className="grid gap-4">
-                            <FormField
-                                control={form.control}
-                                name="bio"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Bio</FormLabel>
-                                        <FormControl>
-                                            <Textarea placeholder="A short bio about yourself" className="resize-none" {...field} />
-                                        </FormControl>
-                                        <FormDescription>Brief description for your profile.</FormDescription>
-                                        <FormMessage {...form.error} />
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name="website"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Website URL</FormLabel>
-                                        <FormControl>
-                                            <Input placeholder="https://example.com" {...field} />
-                                        </FormControl>
-                                        <FormMessage {...form.error} />
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name="location"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Location</FormLabel>
-                                        <FormControl>
-                                            <Input placeholder="City, Country" {...field} />
-                                        </FormControl>
-                                        <FormMessage {...form.error} />
-                                    </FormItem>
-                                )}
-                            />
-                        </div>
-                    </div>
+                            <div className="space-y-4">
+                                <h2 className="text-xl font-semibold">Basic</h2>
+                                <div className="grid gap-4">
+                                    <FormField
+                                        control={form.control}
+                                        name="bio"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Bio</FormLabel>
+                                                <FormControl>
+                                                    <Textarea placeholder="A short bio about yourself" className="resize-none" {...field} />
+                                                </FormControl>
+                                                <FormDescription>Brief description for your profile.</FormDescription>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                    <FormField
+                                        control={form.control}
+                                        name="website"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Website URL</FormLabel>
+                                                <FormControl>
+                                                    <Input placeholder="https://example.com" {...field} className='lowercase' />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                    <FormField
+                                        control={form.control}
+                                        name="location"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Location</FormLabel>
+                                                <FormControl>
+                                                    <Input placeholder="City, Country" {...field} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+                            </div>
 
-                    <Button type="submit" className="w-full">
-                        Save Profile Information
-                    </Button>
-                </form>
-            </Form>
-        </Card>
+                            <Button type="submit" className="w-full">
+                                Save Profile Information
+                            </Button>
+                        </form>
+                    </Form>
+                </Card>
+            )}
+        </>
     );
 }
